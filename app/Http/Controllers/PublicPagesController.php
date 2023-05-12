8423d21bc3725e89f8c04dde0c9f4bb3dcbd7bfb
Ameerali C <ameeralic95@gmail.com>
2023-05-12 15:57:07 +0530

    public folder created

--- app/Http/Controllers/PublicPagesController.php
+++ app/Http/Controllers/PublicPagesController.php
@@ -9,6 +9,6 @@ class PublicPagesController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Home');
+        return Inertia::render('Public/Home');
     }
 }
